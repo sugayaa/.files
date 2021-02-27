d4a3d5c227d51ddbdc4125c25939647717e37d09
sugaya <vitorsugaya5@gmail.com>
2021-02-27 16:12:13 -0300

    polybar multimonitor fix

--- crs/dev/scripts/polybar-launch.sh
+++ crs/dev/scripts/polybar-launch.sh
@@ -6,11 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# waiting for all monitors to come online
+sleep 3
+
 # Launch Polybar, using default config location ~/.config/polybar/config
 # on all monitors available
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -r example & polybar -r example1 &
+    echo "$m" >> /home/sugaya/multiple-monitors
+    MONITOR=$m polybar -r example &
+    sleep 1
+    MONITOR=$m polybar -r example1 &
   done
 else
   polybar -r example & polybar -r example1 &
